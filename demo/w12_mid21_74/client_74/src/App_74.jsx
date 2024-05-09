01c650d6417d1958291d73bcd0f92ebb677811fb
Chen211410674 <211410674@example.com>
2024-05-09 15:35:44 +0800

    w12

---
+++ demo/w12_mid21_74/client_74/src/App_74.jsx
@@ -0,0 +1,60 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+import { HomeLayout_74, HomePage_74 } from './pages';
+
+import SupaGetBlog_74 from './pages/demo_74/SupaGetBlog_74';
+
+import { Mid1SupaBlog_74, Mid2NodeBlogLocal_74, Mid2NodeBlogSupa_74} from './pages/mid1_74';
+
+//import Mid1SupaBlog_74 from './pages/mid1_74/Mid1SupaBlog_74';
+//import Mid2NodeBlog_74 from './pages/mid1_74/Mid2NodeBlog_74';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5, //5minutes
+    },
+  },
+});
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomeLayout_74 />,
+    children: [
+      {
+        index: true,
+        element: <HomePage_74 />,
+      },
+      {
+        path: 'mid1Blog_74',
+        element: <Mid1SupaBlog_74 />,
+      },
+      {
+        path: 'mid2BlogLocal_74',
+        element: <Mid2NodeBlogLocal_74 />,
+      },
+      {
+        path: 'mid2BlogSupa_74',
+        element: <Mid2NodeBlogSupa_74 />,
+      },
+      {
+        path: 'demoGetBlog_74',
+        element: <SupaGetBlog_74 />,
+      },
+    ],
+  },
+]);
+
+const App_74 = () => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools initialIsOpen={true} />
+    </QueryClientProvider>
+  );
+};
+
+export default App_74;
---
+++ demo/w12_mid21_74/client_74/src/pages/HomeLayout_74.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from 'react-router-dom';
+
+import Navbar_74 from '../components/Navbar_74';
+const HomeLayout_74 = () => {
+  return (
+    <>
+      <Navbar_74 />
+      <Outlet />
+    </>
+  );
+};
+
+export default HomeLayout_74;
