41e2e3af541c314a15310045e30b32dd478fff97
Chen211410674 <211410674@example.com>
2024-06-01 23:16:28 +0800

    w15

---
+++ demo/w15_74/src/pages/NewUsers_74.jsx
@@ -0,0 +1,5 @@
+const NewUsers_74 = () => {
+  return <>Create a new user</>;
+};
+
+export default NewUsers_74;
---
+++ demo/w15_74/src/services/apiBookings.js
@@ -0,0 +1,16 @@
+// import supabase from './supabase';
+
+import supabase, { supabaseUrl } from './supabase';
+
+export const getBooking = async (id) => {
+  const { data, error } = await supabase
+    .from('bookings_74')
+    .select('*, cabins_74(*), guests_74(*)')
+    .eq('id', id);
+
+  if (error) {
+    console.error(error);
+    throw new Error('Bookings could not be loaded');
+  }
+  return data;
+};
